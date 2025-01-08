9a376648ffa7516dfc05f78ab8e1945006bc9d76
SurSargsyan <137448149+SurSargsyan@users.noreply.github.com>
2025-01-08 16:21:35 +0100

    Add files via upload

---
+++ include/minitalk_bonus.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk_bonus.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: susargsy <susargsy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/08 16:14:22 by susargsy          #+#    #+#             */
+/*   Updated: 2025/01/08 16:14:25 by susargsy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_BONUS_H
+# define MINITALK_BONUS_H
+
+# include "../printf/ft_printf.h"
+# include <signal.h>
+
+#endif
